286d582775752918743730bc61e4bebe142bf82f
AMITJHA <amitjha11003@gmail.com>
2023-01-27 21:45:21 +0530

    game backand work

---
+++ src/chapter1Program2.java
@@ -0,0 +1,22 @@
+public class chapter1Program2
+{
+   public static void main(String[] args){
+
+System.out.print("CBSE Result");
+int english = 78;
+int hindi = 56;
+int science = 66;
+int maths = 45;
+int sst = 23;
+        System.out.println("science =66");
+        System.out.println("english=78");
+        System.out.println("maths=45");
+        System.out.println("hindi =56");
+        System.out.println("sst =23");
+        int total = english +hindi +maths + science +sst;
+        System.out.println("Total");
+        System.out.println(total);
+        System.out.print("percentage");
+        System.out.println(total/5);
+    }
+}
